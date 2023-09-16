779ac3808d7dfa53b289a98fbd78707c91214a43
GAURAV SHARMA <gsharma1154@gmail.com>
2023-09-16 07:15:54 +0530

    EvenDigits

---
+++ src/LinearSearchAlgo/EvenDigits.java
@@ -0,0 +1,41 @@
+package LinearSearchAlgo;
+//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+public class EvenDigits
+{
+
+    static int findNumbersWithEvenNoOfDigits(int[] nums)
+    {
+       int count=0;
+        for (int x:nums
+             ) {
+if (even(x)){
+    count++;
+}
+        }
+        return count;
+    }
+
+   //fn to check whether a number contains even no of digits or not
+    private static boolean even(int x) {
+        return noOfDigits(x) % 2 == 0;
+    }
+    private static int noOfDigits(int x) {
+        //two ways to count no of digits in number:
+        //1. count the no of digits
+        //2.convert into string and count the length
+        int count=0;
+        while(x>0){
+            count++;
+            x=x/10;
+
+        }
+        return count;}
+
+    public static void main(String[] args) {
+        int[] arr={12,345,2,6,7896};
+        System.out.println(findNumbersWithEvenNoOfDigits(arr));
+    }
+    }
+
+
+
