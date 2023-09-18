8e594b59ee2d3dc5d4b52c8de44442aeec00cde8
GAURAV SHARMA <gsharma1154@gmail.com>
2023-09-18 20:10:12 +0530

    RunningSum or prefix sum

--- src/ArrayAndArrayList/RunningSum.java
+++ src/ArrayAndArrayList/RunningSum.java
@@ -2,18 +2,14 @@ package ArrayAndArrayList;
 
 import java.util.Arrays;
 
+//prefix sum
 public class RunningSum {
     public static int[] runningSum(int[] nums) {
 int[] arr=new int[nums.length];
 arr[0]=nums[0];
+
         for (int i = 1; i < nums.length; i++) {
-            int sum=0;
-           while (i!=0)
-           {
-               sum=sum+nums[i]+nums[i-1];
-               i--;
-           }
-           arr[i]=sum;
+            arr[i]=arr[i-1]+nums[i];
         }
 return arr;
     }
