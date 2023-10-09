2f663449315f09728b01ea1fdd135892e092aeb5
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-09 21:10:47 +0530

    ReArrangingArrayInIncreasingAndDecreasingOrder

---
+++ src/TcsNqtStriverSheet/Array/ReArrangingArrayInIncreasingAndDecreasingOrder.java
@@ -0,0 +1,28 @@
+package TcsNqtStriverSheet.Array;
+
+import java.util.Arrays;
+
+public class ReArrangingArrayInIncreasingAndDecreasingOrder {
+    //rearrange the first half in increasing and second half in decreasing
+
+
+
+    public static void main(String[] args)
+    {
+        int[] arr={22,5,44,22,76,2,126,1,0};
+reArrange(arr);
+    }
+    public static void reArrange(int[] arr){
+        Arrays.sort(arr);
+
+        for (int i = 0; i <= arr.length/2; i++) {
+            System.out.print(arr[i]+" ");
+
+        }
+        for (int i = arr.length-1; i > arr.length/2 ; i--)
+        {
+            System.out.print(arr[i]+" ");
+
+        }
+    }
+}
