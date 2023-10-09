500d9497a2f3b14fb35d977603a38b243c5ea015
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-10 01:40:15 +0530

    SumOfAllElementsInAnArray

---
+++ src/TcsNqtStriverSheet/Array/SumOfAllElementsInAnArray.java
@@ -0,0 +1,18 @@
+package TcsNqtStriverSheet.Array;
+
+//using for loop
+//tc:O(N) as 1 loop is used
+//sc:O(1)
+public class SumOfAllElementsInAnArray {
+    public static void main(String[] args) {
+        int[] arr={33,1,2,4,5,-5};
+        System.out.println(sum(arr));
+
+    }public static int sum(int[] arr){
+        int count=0;
+        for(int x:arr){
+            count=count+x;
+        }
+        return count;
+    }
+}
