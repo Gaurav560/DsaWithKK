2bbcbae1e52d19e9597e19c7448584651eeea130
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-09 15:02:25 +0530

    ReverseAnArrayTwoPointerApproach

--- src/TcsNqtStriverSheet/Array/ReverseAnArrayTwoPointerApproach.java
+++ src/TcsNqtStriverSheet/Array/ReverseAnArrayTwoPointerApproach.java
@@ -3,6 +3,8 @@ package TcsNqtStriverSheet.Array;
 import java.util.Arrays;
 
 public class ReverseAnArrayTwoPointerApproach {
+    //time complexity:O(N) as single pass or loop is involved
+    //space complexity: O(1)
     public static void main(String[] args) {
 int[] arr={2,5,6,8,5,3,1,-4,0};
         System.out.println(Arrays.toString(twoPointer(arr)));
