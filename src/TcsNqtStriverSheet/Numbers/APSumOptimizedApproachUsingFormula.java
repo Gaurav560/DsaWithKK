0573691b59e6d6a25a5b1625dff59e55336077c7
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-12 21:20:23 +0530

    APSumOptimizedApproachUsingFormula

---
+++ src/TcsNqtStriverSheet/Numbers/APSumOptimizedApproachUsingFormula.java
@@ -0,0 +1,13 @@
+package TcsNqtStriverSheet.Numbers;
+
+//tc:O(1) and sc:O(1)
+public class APSumOptimizedApproachUsingFormula {
+    public static void main(String[] args) {
+        double n=4; double a=2; double d=2;
+        double sum=(n/2)*(2*a+((n-1)*d));
+        System.out.println(sum);
+    }
+}
+
+
+
