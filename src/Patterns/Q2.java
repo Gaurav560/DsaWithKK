c3b6065e32190d3cace34a5373ed3fdb4db866ef
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-16 23:53:04 +0530

    Q2

---
+++ src/Patterns/Q2.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class Q2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
