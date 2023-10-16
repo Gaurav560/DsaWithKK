6aa1e11bc285a9a6edc56fb6cc44ddd6335a9c26
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-17 00:10:35 +0530

    Q7

---
+++ src/Patterns/Q7.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Q7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for (int j = 5; j >=i ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
