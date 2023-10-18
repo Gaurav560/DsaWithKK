62ecc1a78c3cd18f5601a5a895611c3806c68a0c
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-18 18:15:25 +0530

    Q16

---
+++ src/Patterns/Q16.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Q16 {
+    public static void main(String[] args) {
+        char ch='A';
+        for (int i = 0; i < 5; i++) {
+
+
+            for (int j = 0; j <=i; j++) {
+                System.out.print(ch+" ");
+
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
