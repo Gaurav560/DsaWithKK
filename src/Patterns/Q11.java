66833f9dc9e408ed0bfcf9c640f59cd55c459bdb
GAURAV SHARMA <gsharma1154@gmail.com>
2023-10-18 17:00:04 +0530

    Q11

---
+++ src/Patterns/Q11.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Q11 {
+    public static void main(String[] args) {
+        int start=1;
+        for (int i = 0; i <5 ; i++) {
+            if(i%2==0){start=1;}else {start=0; }
+            for (int j = 0; j <=i; j++) {
+                System.out.print(start);
+                start=1-start;
+
+
+            }
+            System.out.println();
+        }
+    }
+}
