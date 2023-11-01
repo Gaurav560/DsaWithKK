19247d1804a5179b38275f218947ea0635e82d04
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-02 00:24:10 +0530

    Q8

---
+++ src/StriverSheetDsa/LearnTheBasics/Patterns/Q8.java
@@ -0,0 +1,16 @@
+package StriverSheetDsa.LearnTheBasics.Patterns;
+
+public class Q8 {
+    public static void main(String[] args) {
+        for (int i = 5; i >=1; i--) {
+            for (int j = i; j <5 ; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <=2*i-1; j++) {
+                System.out.print(" "+"*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
