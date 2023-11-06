84f66f4fe9ec31bdd95161b1c6f062948c468f0f
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-07 02:26:34 +0530

    Q6

---
+++ src/StriverSheetDsa/LearnTheBasics/RecursionBasics/Q6.java
@@ -0,0 +1,12 @@
+package StriverSheetDsa.LearnTheBasics.RecursionBasics;
+
+public class Q6 {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+    public static int factorial(int n){
+        if (n==1)return 1;
+        return n*factorial(n-1);
+    }
+
+}
